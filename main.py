ca051e5894b1de7f7bc3e1244ed4b1709dea8d22
Alexis Valentino <45990091+alexisvalentino@users.noreply.github.com>
2023-03-19 23:00:59 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import datetime
+import time
+from plyer import notification
+from alpha_vantage.timeseries import TimeSeries
+
+api_key = '<your_api_key>'
+ts = TimeSeries(key=api_key)
+
+while True:
+    data, metadata = ts.get_quote_endpoint(symbol='TSLA')
+
+    notification.notify(
+        title="TSLA data ({})".format(datetime.date.today()),
+        message="Current Price = {}, \nDayLow = {}, \nDayHigh ={}".format(
+            data['05. price'],
+            data['04. low'],
+            data['03. high']
+        ),
+        app_icon="C:/Users/Desktop/image.ico",
+        timeout=5
+    )
+
+    time.sleep(60*60*2)
